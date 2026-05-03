bd5879c701ab388d6d20d337710cc2d3d2c9970c
safayet404 <hossainsafayet187@gmail.com>
2026-05-04 00:02:41 +0600

    login issued

--- apps/restapi/src/app.ts
+++ apps/restapi/src/app.ts
@@ -55,6 +55,41 @@ app.post('/auth/register', async (req, res) => {
     }
 })
 
+app.post('/auth/login', async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            res.status(400).json({
+                message: "Email and password are required"
+            })
+        }
+
+        const user = await Promise.resolve(
+            db.chain.get('users').find({ email }).value()
+        )
+
+        if (!user) {
+            res.status(401).json({
+                message: "Invalid Credentials"
+            })
+        }
+
+        const hashedPassowrd = `hashed_${password}`
+
+        if (user.password !== hashedPassowrd) {
+            res.status(401).json({
+                message: 'Invalid credentials'
+            })
+        }
+        const dummyToken = 'dummy_token_give_me'
+        res.status(200).json({
+            dummyToken
+        })
+    } catch (error) {
+
+    }
+})
 
 
 app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
